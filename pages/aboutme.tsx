a718ad88d46f0bb2f3ff2e1bfc1853ab641dcd43
laola1412 <privat.nilsarndt@gmail.com>
2023-05-07 04:07:20 +0200

    create first iteration of aboutme page

--- pages/aboutme.tsx
+++ pages/aboutme.tsx
@@ -4,32 +4,71 @@ export default function aboutme() {
     return (
         <>
             <motion.div
-                className="container p-4 mx-auto text-gray-700 page-vertical-center"
-                animate={{ opacity: 1 }}
+                animate={{ opacity: 1, transition: { duration: 0.4 } }}
                 initial={{ opacity: 0 }}
                 exit={{ opacity: 0 }}
             >
-                <div className="grid mb-24 tracking-wide text-center justify-self-center">
-                    <p className="mb-4 text-6xl font-medium">About Me</p>
-                    <p className="text-2xl">Vita, Experiences & Roadmap </p>
-                </div>
-                <div className="grid grid-cols-4 gap-8 text-gray-100">
-                    <div className="h-full overflow-hidden border-2 border-gray-200 rounded-lg border-opacity-60 hover:cursor-pointer">
-                        <img
-                            className="object-cover object-center w-full lg:h-48 md:h-24"
-                            src="https://assets.simpleviewinc.com/simpleview/image/fetch/c_fill,h_632,q_75,w_640/https://assets.simpleviewinc.com/simpleview/image/upload/crm/johnstoncounty/Hibbett-Sports---2-2000x1500-72dpi_7229DE23-5056-B3A8-497239E54F6241AD-7229dab35056b3a_7229ecbd-5056-b3a8-491bd5682a7a9ad7.jpg"
-                            alt=""
-                        />
-                        <div className="p-6 bg-red-500">
-                            <h1 className="mb-1 text-sm font-semibold underline">Stock Analysis</h1>
-                            <h1 className="mb-3 text-2xl font-semibold">Hibbett Inc.</h1>
-                            <p className="mb-3 leading-relaxed">
-                                In this stock analysis, I took a closer look at Hibbett. The company
-                                has a lot of catalysts and is priced in with low expectations.
-                            </p>
+                <section className="text-gray-700 bg-orange-100">
+                    <div className="container grid px-8 py-24 mx-auto tracking-wide">
+                        <p className="mb-4 text-6xl font-medium">About Me</p>
+                        <p className="text-2xl">Vita, Experiences & Certificates </p>
+                    </div>
+                </section>
+                <section className="text-gray-700">
+                    <div className="container px-8 py-24 mx-auto ">
+                        <div className="flex">
+                            <div className="pt-2 pr-12 text-lg text-right text-gray-500 border-r-2 border-gray-200 basis-1/5">
+                                <p>currently</p>
+                                <p className="">-</p>
+                                <p>03/2023</p>
+                            </div>
+                            <div className="pt-2 pl-12 basis-4/5">
+                                <h2 className="pb-6 text-4xl font-bold">
+                                    Master of Science Economics
+                                </h2>
+                                <p className="pb-20 text-lg text-gray-500">TU Dortmund</p>
+                            </div>
+                        </div>
+                        <div className="flex">
+                            <div className="pt-2 pr-12 text-lg text-right text-gray-500 border-r-2 border-gray-200 basis-1/5">
+                                <p>currently</p>
+                                <p className="">-</p>
+                                <p>10/2021</p>
+                            </div>
+                            <div className="pt-2 pl-12 basis-4/5">
+                                <h2 className="pb-6 text-4xl font-bold">
+                                    Bachelor of Science Applied Computer Science
+                                </h2>
+                                <p className="pb-20 text-lg text-gray-500">TU Dortmund</p>
+                            </div>
+                        </div>
+                        <div className="flex">
+                            <div className="pt-2 pr-12 text-lg text-right text-gray-500 border-r-2 border-gray-200 basis-1/5">
+                                <p>03/2023</p>
+                                <p className="">-</p>
+                                <p>10/2019</p>
+                            </div>
+                            <div className="pt-2 pl-12 basis-4/5">
+                                <h2 className="pb-6 text-4xl font-bold">
+                                    Bachelor of Science Economics
+                                </h2>
+                                <p className="pb-20 text-lg text-gray-500">TU Dortmund</p>
+                            </div>
+                        </div>
+
+                        <div className="flex">
+                            <div className="pt-2 pr-12 text-lg text-right text-gray-500 border-r-2 border-gray-200 basis-1/5">
+                                <p>08/2018</p>
+                                <p className="">-</p>
+                                <p>08/2010</p>
+                            </div>
+                            <div className="pt-2 pl-12 basis-4/5">
+                                <h2 className="pb-6 text-4xl font-bold">Abitur</h2>
+                                <p className="pb-20 text-lg text-gray-500">Mariengymnasium Werl</p>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </section>
             </motion.div>
         </>
     )
